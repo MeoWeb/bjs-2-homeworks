78401c70e8c95af604dd6acb03d565b9837bdac3
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-10-02 14:40:14 +0300

    task.js updated

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,44 +1,48 @@
-'use strict'
+"use strict";
 
 function solveEquation(a, b, c) {
-  let arr = [];
-  let D = b ** 2 - 4 * a * c;
+    let arr = [];
+    let D = b ** 2 - 4 * a * c;
 
-  if (D < 0) {
+    if (D == 0) {
+        arr.push(-b / (2 * a));
+    } else if (D > 0) {
+        arr.push((-b + Math.sqrt(D)) / (2 * a));
+        arr.push((-b - Math.sqrt(D)) / (2 * a));
+    }
     return arr;
-  } else if (D == 0) {
-    arr.push(-b / (2 * a));
-    return arr;
-  } else {
-    arr.push((-b + Math.sqrt(D)) / (2 * a));
-    arr.push((-b - Math.sqrt(D)) / (2 * a))
-    return arr;
-  }
 }
 
-
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let S = amount - contribution;
-  let P = percent * (1 / 12);
-  let today = new Date();
+    let S = amount - contribution;
+    let P = (percent * (1 / 12)) / 100;
+    let today = new Date();
 
-  percent = Number(percent);
-  contribution = Number(contribution);
-  amount = Number(amount);
-  percent = percent / 100;
+    percent = Number(percent);
+    contribution = Number(contribution);
+    amount = Number(amount);
 
-  if (percent.isNan || contribution.isNan || amount.isNan || percent <= 0 || contribution < 0 || amount <= 0) {
-    return 'Ошибка';
-  }
+    if (
+        isNaN(percent) ||
+        isNaN(contribution) ||
+        isNaN(amount) ||
+        percent <= 0 ||
+        contribution < 0 ||
+        amount <= 0
+    ) {
+        return `Параметр содержит неправильное значение`;
+    }
 
-  let checkDateYear = date.getFullYear() - today.getFullYear();
-  let checkDateMonth = date.getMonth() - today.getMonth();
+    let checkDateYear = date.getFullYear() - today.getFullYear();
+    let checkDateMonth = date.getMonth() - today.getMonth();
 
-  if (checkDateYear < 0) { return 'Ошибка'; }
+    if (checkDateYear < 0) {
+        return `Ошибка`;
+    }
 
-  let numberOfMonths = checkDateMonth + (checkDateYear * 12);
-  let monthAmount = S * (P + P / (((1 + P) ** numberOfMonths) - 1));
-  let totalAmount = monthAmount * numberOfMonths;
+    let numberOfMonths = checkDateMonth + checkDateYear * 12;
+    let monthAmount = S * (P + P / ((1 + P) ** numberOfMonths - 1));
+    let totalAmount = monthAmount * numberOfMonths;
 
-  return Number(totalAmount.toFixed(2));
+    return Number(totalAmount.toFixed(2));
 }
