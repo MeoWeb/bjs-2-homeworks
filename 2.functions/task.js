182a7698f7d3bb8ea9ad7d861f42ca7255df2025
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-11-03 16:05:35 +0300

    add task.js 1,2,3

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -36,7 +36,7 @@ function worker(arr) {
   return sum;
 }
 
-function makeWork(arrOfArr, func) {
+function makeWork(arrOfArr, worker, worker2) {
   let max = 0;
 
   for (let i = 0; i < arrOfArr.length; i += 1) {
@@ -49,5 +49,18 @@ function makeWork(arrOfArr, func) {
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min = arr[0];
+  let max2 = arr[0];
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (arr[i] > max2) {
+      max2 = arr[i];
+    }
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+  }
+  let diff = max2 - min;
+
+  return diff;
 }
