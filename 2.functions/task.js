91e20e1a449676da711b1afa1fb665b4d76f3e16
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-11-03 15:36:39 +0300

    Add files via upload

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -3,21 +3,21 @@ function getArrayParams(arr) {
   let min = arr[0];
   let max = arr[0];
   let sum = 0;
-  let avg;
+
 
   for (let i = 0; i < arr.length; i += 1) {
 
     if (arr[i] > max) {
       max = arr[i];
     }
-
     if (arr[i] < min) {
       min = arr[i];
     }
 
     sum += arr[i];
-    avg = sum / arr.length;
   }
+
+  let avg = sum / arr.length;
 
   return {
     min: min,
@@ -26,22 +26,24 @@ function getArrayParams(arr) {
   };
 }
 
-
 // Задание 2
 function worker(arr) {
-  let sum;
+  let sum = 0;
 
-  // Ваш код
-
+  for (let i = 0; i < arr.length; i += 1) {
+    sum += arr[i];
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = 0;
 
-  // Ваш кода
-  // for ...
-
+  for (let i = 0; i < arrOfArr.length; i += 1) {
+    if (worker(arrOfArr[i]) > max) {
+      max = worker(arrOfArr[i]);
+    }
+  }
   return max;
 }
 
