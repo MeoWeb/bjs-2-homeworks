68f70fa161ee2fc7b208f9a6787483d42163dec0
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-11-23 19:43:24 +0300

    task js 1,2

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
+    let result = arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i]);
 
-  // Ваш код
-
-  return result; // boolean
+    return result;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
+    let resultArr = arr.filter(function(i) { return i > 0 && i % 3 == 0 }).map(function(i) { return i * 10 });
 
-  // Ваш код
-
-  return resultArr; // array
+    return resultArr;
 }
