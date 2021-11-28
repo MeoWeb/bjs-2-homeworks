72da100951afb194f2b1c893b8973ede68ace271
Masha <70323469+MeoWeb@users.noreply.github.com>
2021-11-28 18:39:48 +0300

    task.js 1

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,37 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+};
 
+
+Student.prototype.setSubject = function(subjectName) { this.subject = subjectName };
+
+Student.prototype.addMark = function(mark) {
+
+    if (this.marks === undefined) {
+        this.marks = [];
+        this.marks.push(mark);
+    } else {
+        this.marks.push(mark);
+    }
+};
+
+Student.prototype.addMarks = function(...mark) { this.marks = mark };
+
+Student.prototype.getAverage = function(marks) {
+
+    let sum = 0;
+
+    for (let i = 0; i < this.marks.length; i++) {
+        sum += this.marks[i];
+    }
+    return sum / this.marks.length;
+};
+
+
+Student.prototype.exclude = function(reason) {
+    delete Student.subject;
+    delete Student.marks;
+    this.excluded = reason;
 }
-
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
-
-// ваш код для остальных методов
