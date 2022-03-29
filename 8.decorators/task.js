e2c7b47f344f95d4e54b82a0869622f3d4733531
Masha <70323469+MeoWeb@users.noreply.github.com>
2022-03-29 20:02:09 +0300

    task js upd

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,5 +1,5 @@
 function cachingDecoratorNew(func) {
-    let cache = [];
+    let cache = {};
 
     function wrapper(...args) {
         const hash = args.join(',');
@@ -9,9 +9,9 @@ function cachingDecoratorNew(func) {
         } else {
             let result = func.call(this, ...args);
             cache[hash] = result;
-
-            if (cache.length > 5) {
-                cache.shift();
+            if (Object.keys(cache).length > 5) {
+                const key = Object.keys(cache);
+                delete cache[key[0]];
             }
             console.log("Вычисляем: " + result);
             return "Вычисляем: " + result;
@@ -21,10 +21,41 @@ function cachingDecoratorNew(func) {
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+    let timer;
+    let flag = false;
+
+    return function wrapper(...args) {
+        if (!flag) {
+            flag = true;
+            timer = setTimeout(() => {
+                func(...args)
+                flag = false;
+            }, ms);
+        } else {
+            clearTimeout(timer);
+            timer = setTimeout(() => func(...args), ms);
+        }
+    }
 }
 
+
 function debounceDecorator2(func) {
-  // Ваш код
+    let timer;
+    let flag = false;
+
+    return function wrapper(...args) {
+        if (!flag) {
+            flag = true;
+            timer = setTimeout(() => {
+                flag = false
+                wrapper.count.push()
+                return func.call(this, ...args)
+            }, ms);
+        } else {
+            clearTimeout(timer);
+            timer = setTimeout(() => func(...args), ms);
+        }
+        wrapper.count = [];
+    }
 }
