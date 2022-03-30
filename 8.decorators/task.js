cf8791c4ad9abddf1706468938a57ce3f65a74c9
Masha <70323469+MeoWeb@users.noreply.github.com>
2022-03-30 21:02:22 +0300

    UPD

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,61 +1,57 @@
 function cachingDecoratorNew(func) {
     let cache = {};
 
-    function wrapper(...args) {
+    return function wrapper(...args) {
         const hash = args.join(',');
         if (hash in cache) {
             console.log("Из кеша: " + cache[hash]);
-            return "Из кэша: " + cache[hash];
         } else {
             let result = func.call(this, ...args);
             cache[hash] = result;
-            if (Object.keys(cache).length > 5) {
-                const key = Object.keys(cache);
-                delete cache[key[0]];
-            }
             console.log("Вычисляем: " + result);
-            return "Вычисляем: " + result;
+        }
+        if (Object.keys(cache).length > 5) {
+            const key = Object.keys(cache);
+            delete cache[key[0]];
         }
     }
-    return wrapper;
 }
-
 
 function debounceDecoratorNew(func, ms) {
     let timer;
     let flag = false;
 
     return function wrapper(...args) {
+
         if (!flag) {
+            func(...args);
             flag = true;
-            timer = setTimeout(() => {
-                func(...args)
-                flag = false;
-            }, ms);
         } else {
-            clearTimeout(timer);
             timer = setTimeout(() => func(...args), ms);
+            ms += 2000;
         }
+
     }
 }
 
 
-function debounceDecorator2(func) {
+function debounceDecorator2(func, ms) {
     let timer;
+    let count = 0;
     let flag = false;
+
 
     return function wrapper(...args) {
         if (!flag) {
+            func(...args);
             flag = true;
-            timer = setTimeout(() => {
-                flag = false
-                wrapper.count.push()
-                return func.call(this, ...args)
-            }, ms);
+            count += 1;
+            console.log(count);
         } else {
-            clearTimeout(timer);
             timer = setTimeout(() => func(...args), ms);
+            ms += 2000;
+            count += 1;
+            console.log(count);
         }
-        wrapper.count = [];
     }
 }
